16afe42773f16790b8e34c95a8dcd877469f6cb7
Nguyen Quoc Nam <nguyenquocnam612@gmail.com>
2025-07-08 11:34:04 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,35 +48,30 @@ async function runBotLoot() {
             
             await page.waitForNavigation({ waitUntil: "networkidle2" });
             await page.select("#UniversityId", "94");
-            console.log("Selected DH BKHCM");
 
+            await new Promise(res => setTimeout(res, 400));
             await page.waitForSelector("#Gender", { visible: true });
             await page.select("#Gender", "false");
-            console.log("Selected Nam");
 
 
-            await new Promise(res => setTimeout(res, 300));
+            await new Promise(res => setTimeout(res, 400));
             await page.waitForSelector("#DormitoryAreaId", { visible: true });
             await page.select("#DormitoryAreaId", "4");
-            console.log("Selected khu B");
 
-            await new Promise(res => setTimeout(res, 300));
+            await new Promise(res => setTimeout(res, 400));
             await page.waitForSelector("#DormitoryRoomTypeId", { visible: true });
             await page.select("#DormitoryRoomTypeId", "3");
-            console.log("Selected Dich vu 4 sv");
 
-            await new Promise(res => setTimeout(res, 300));
+            await new Promise(res => setTimeout(res, 400));
             await page.waitForSelector("#DormitoryHouseId", { visible: true });
             await page.select("#DormitoryHouseId", "57");
-            console.log("Selected E04");
 
-            await new Promise(res => setTimeout(res, 300));
+            await new Promise(res => setTimeout(res, 400));
             await page.waitForSelector("a[data-target='#floor-tab5']", { visible: true });
             await page.click('a[data-target="#floor-tab5"]');
-            console.log("Selected Tang 5");
 
             // Check Room 512
-            await new Promise(res => setTimeout(res, 300));
+            await new Promise(res => setTimeout(res, 400));
             const roomSelector = ".dormitory-room-item.dormitory-room-item-6563";
             const room = await page.waitForSelector(roomSelector, {
                 visible: true,
@@ -85,29 +80,33 @@ async function runBotLoot() {
 
             if (room) {
                 await room.click();
-                console.log("Selected room 512");
 
-                await new Promise(res => setTimeout(res, 100));
+                await new Promise(res => setTimeout(res, 200));
                 await page.waitForSelector("input#rent-item-51640", { visible: true });
                 await page.click("input#rent-item-51640");
-                console.log("Tried selecting slot 4");
 
-                await new Promise(res => setTimeout(res, 100));
+                await new Promise(res => setTimeout(res, 200));
                 await page.waitForSelector("a.btn.btn-continue", { visible: true });
                 const dialogPromise = waitForDialog(page, "Vui lòng chọn giường!", 1000);
                 await page.click("a.btn.btn-continue");
                 const noSlot = await dialogPromise;
 
                 if (noSlot) {
-                    console.log("⚠️ No bed selected, retrying in 5s...");
                     await new Promise(res => setTimeout(res, 5000));
                     continue;
                 }
                 console.log("Selected empty slot 4");
+                
+                const reachedRegistryPanel = await page.waitForSelector("#StudentInfo", {
+                    visible: true,
+                    timeout: 10000
+                }).catch(() => null);
 
-                await new Promise(res => setTimeout(res, 100));
-                const timesUp = await waitForDialog(page, "Đã hết thời gian đăng ký", 22 * 60000);
-                if (timesUp) console.log("Loop completed, retrying");
+                if (reachedRegistryPanel) {
+                    console.log("✅ Registry panel reached");
+                    break;
+                } 
+                else console.log("❌ Failed to reach registry panel. Retrying...");
             }
             else console.log("Room 512 is NOT available, retrying...");
         }
